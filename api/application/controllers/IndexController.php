b85f842609a4cbbc955e566d4f7f3ac355cd4ccc
Rodrigo Silveira <rsilveira@deseretdigital.com>
2014-12-11 13:49:43 -0700

    Ad service

--- api/application/controllers/IndexController.php
+++ api/application/controllers/IndexController.php
@@ -2,15 +2,26 @@
 
 class IndexController extends Zend_Controller_Action
 {
+    /** @var Application_Service_Ad service */
+    protected $service;
+
     public function init()
     {
-//        $this->serviceFactory = new ServiceFactory();
+        $this->service = new Application_Service_Ad();
     }
 
     public function indexAction()
     {
-        $hello = ['hello' => 'MONAD'];
+        $hello = [
+            'service' => 'MONAD',
+            'testNum' => $this->service->test()
+        ];
 
         $this->_helper->getHelper('json')->sendJson($hello);
+    }
+
+    public function getQuestion()
+    {
+        
     }
 }
